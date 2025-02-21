4c427b4107367d87b43be3623cf549c1428f7856
harshwardhanpatil-8010 <harshwardhan.patil2005@gmail.com>
2025-02-21 22:32:40 +0530

    new

--- Frontend/src/components/landing.jsx
+++ Frontend/src/components/landing.jsx
@@ -17,17 +17,17 @@ const Landing = () => {
       <div className="absolute inset-0 flex items-center pl-6 sm:pl-10 md:pl-16 lg:pl-24 xl:pl-32 2xl:pl-40 text-left bg-black/30">
         <div className="max-w-xs sm:max-w-sm md:max-w-md lg:max-w-lg xl:max-w-xl 2xl:max-w-2xl">
           <div className="flex items-center gap-2 sm:gap-4 whitespace-nowrap">
-            <h1 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl xl:text-5xl 2xl:text-6xl 3xl:text-6xl font-bold text-white mb-2">
+            <h1 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl xl:text-4xl 2xl:text-6xl 3xl:text-6xl font-bold text-white mb-2">
               <b className="text-[#aca6ff]">A</b>mazon <b className="text-[#aca6ff]">W</b>eb <b className="text-[#aca6ff]">S</b>ervices
             </h1>
           </div>
           <div className="flex items-center gap-2 sm:gap-4 whitespace-nowrap">
-            <h2 className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl 2xl:text-[54px] 3xl:text-[54px] font-bold text-gray-300">
+            <h2 className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-[54px] 2xl:text-[54px] 3xl:text-[54px] font-bold text-gray-300">
               International Cloud Clubs
             </h2>
           </div>
           <div className="flex items-center mt-2 sm:mt-3 whitespace-nowrap">
-            <h3 className="text-lg sm:text-xl md:text-2xl lg:text-3xl xl:text-4xl 2xl:text-3xl 3xl:text-4xl font-thin text-gray-300">
+            <h3 className="text-lg sm:text-xl md:text-2xl lg:text-3xl xl:text-2xl 2xl:text-3xl 3xl:text-4xl font-thin text-gray-300">
               @MIT WPU, Kothrud
             </h3>
           </div>
